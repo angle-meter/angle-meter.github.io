87bcf4b98a1040a069551b3a5bceb76bb5bfbddd
xantorohara <xantorohara@local>
2019-02-10 13:27:13 +0300

    translations

--- scripts.js
+++ scripts.js
@@ -1,28 +1,37 @@
 window.onload = function () {
-    var svg = Snap('#svg');
+    var svg = Snap('svg');
 
     var SIZE = svg.attr('viewBox').width;
     var CENTER = SIZE / 2;
 
-    var line = svg.select('#line');
-    var marker = svg.select('#marker');
-    var text = svg.select('#text');
+    var pointerLine = svg.select('#pointerLine');
+
+    var pointerMarker = svg.select('#pointerMarker');
+    var pointerText = svg.select('#pointerText');
     var arc = svg.select('#arc');
+    var arcText = svg.select('#arcText');
     var oppositeArc = svg.select('#oppositeArc');
-    var oppositeText = svg.select('#oppositeText');
-    var positiveText = svg.select('#positiveText');
+    var oppositeArcText = svg.select('#oppositeArcText');
 
     var input = document.getElementById('input');
 
-    ['click', 'keyup'].forEach(function (type) {
-        input.addEventListener(type, function () {
-            animateAngle(input.value|0);
+    function addMouseListener() {
+        var pt = svg.node.createSVGPoint();
+        svg.mousedown(function (e) {
+            pt.x = e.clientX;
+            pt.y = e.clientY;
+            var pos = pt.matrixTransform(svg.node.getScreenCTM().inverse());
+            setAngle(calcAngle(pos.x - CENTER, CENTER - pos.y));
         });
-    });
+    }
 
-    svg.mousedown(function (e) {
-        setAngle(calcAngle(e.offsetX - svg.node.clientWidth / 2, svg.node.clientHeight / 2 - e.offsetY));
-    });
+    function addInputListener() {
+        ['click', 'keyup'].forEach(function (type) {
+            input.addEventListener(type, function () {
+                animateAngle(input.value | 0);
+            });
+        });
+    }
 
     function calcAngle(x, y) {
         var angle = Math.round(Math.atan2(y, x) * 180 / Math.PI);
@@ -35,11 +44,14 @@ window.onload = function () {
     }
 
     function animateAngle(angle) {
-        drawLine(angle);
-        drawArc(angle);
-        drawOppositeArc(angle);
-        drawOppositeText(angle);
-        drawText(angle);
+        var normAngle = angle % 360;
+        drawPointerLine(normAngle);
+        drawPointerTriangle(normAngle);
+        drawPointerText(normAngle, angle);
+        drawArc(normAngle);
+        drawArcText(normAngle, angle + '°');
+        drawOppositeArc(normAngle);
+        drawOppositeArcText(normAngle, (angle - 360) + '°');
     }
 
     function calcPos(angle, length) {
@@ -50,26 +62,18 @@ window.onload = function () {
         }
     }
 
-    function drawLine(angle) {
+    function drawPointerLine(angle) {
         var pos = calcPos(angle, CENTER - 3);
-        line.animate({x2: pos.x, y2: pos.y}, 1000, mina.bounce);
+        pointerLine.animate({x2: pos.x, y2: pos.y}, 1000, mina.bounce);
     }
 
-    function drawText(angle) {
+    function drawPointerText(angle, text) {
         var pos = calcPos(angle, CENTER - 1);
-        text.attr({text: angle});
-        text.animate({x: pos.x, y: pos.y}, 1000, mina.bounce);
-
-
-        var pos = calcPos(angle / 2, 10);
-        positiveText.attr({text: angle + '°'});
-        positiveText.animate({x: pos.x, y: pos.y}, 1000, mina.bounce);
-
-        drawTriangles(angle);
+        pointerText.attr({text: text});
+        pointerText.animate({x: pos.x, y: pos.y}, 1000, mina.bounce);
     }
 
-    function drawTriangles(angle) {
-        console.log(d);
+    function drawPointerTriangle(angle) {
         var pos0 = calcPos(angle, CENTER - 3);
         var pos1 = calcPos(angle - 2, CENTER - 2);
         var pos2 = calcPos(angle + 2, CENTER - 2);
@@ -81,22 +85,16 @@ window.onload = function () {
             'Z'
         ].join('');
 
-        marker.animate({d: d}, 1000, mina.bounce);
+        pointerMarker.animate({d: d}, 1000, mina.bounce);
     }
 
-    function drawOppositeText(angle) {
-        if (angle > 180) {
-            angle = angle - 360;
-            var pos = calcPos(angle / 2, 6);
-            oppositeText.attr({text: angle + '°'});
-            oppositeText.animate({x: pos.x, y: pos.y}, 1000, mina.bounce);
-        } else {
-            oppositeText.attr({text: ''});
-            oppositeText.animate({x: CENTER, y: CENTER}, 1000, mina.bounce);
-        }
-    }
 
     function drawArc(angle) {
+        if (angle === 0) {
+            arc.animate({d: 'M' + CENTER + ',' + CENTER}, 1000, mina.bounce);
+            return;
+        }
+
         var size1 = 12;
         var pos = calcPos(angle, size1);
         var size2 = 8;
@@ -133,6 +131,29 @@ window.onload = function () {
         }
     }
 
+    function drawArcText(angle, text) {
+        if (angle >= 10) {
+            var pos = calcPos(angle / 2, 10);
+            arcText.attr({text: text});
+            arcText.animate({x: pos.x, y: pos.y}, 1000, mina.bounce);
+        } else {
+            arcText.attr({x: CENTER, y: CENTER, text: ''});
+        }
+    }
+
+    function drawOppositeArcText(angle, text) {
+        if (angle > 180 && angle <= 350) {
+            angle = angle - 360;
+            var pos = calcPos(angle / 2, 6);
+            oppositeArcText.attr({text: text});
+            oppositeArcText.animate({x: pos.x, y: pos.y}, 1000, mina.bounce);
+        } else {
+            oppositeArcText.attr({x: CENTER, y: CENTER, text: ''});
+        }
+    }
+
+    addMouseListener();
+    addInputListener();
     setAngle(Math.round(Math.random() * 360));
     // setAngle(225);
 };
